4f2fe8170e379fb48e2126863f8fc6b5a7619cf0
Ítalo <italo.ts4895@gmail.com>
2023-04-03 19:24:52 -0300

    Fix placed step count calculation

--- Assets/Scripts/GameSessionController.cs
+++ Assets/Scripts/GameSessionController.cs
@@ -33,8 +33,8 @@ namespace LavaUmaMao {
         private void ResetGame() {
             selectedSlot.Value = null;
             draggedSlot.Value = null;
-            placedStepsCount.Value = 0;
             slotShuffler.ResetGame();
+            placedStepsCount.Value = 0;
             blockInput.Value = false;
         }
 
@@ -72,7 +72,6 @@ namespace LavaUmaMao {
         private IEnumerator WrongAnswerCoroutine() {
             yield return wrongAnswerDelay;
             int wrongSlotsCount = slotShuffler.ResetWrongSlots();
-            placedStepsCount.DecrementValue(wrongSlotsCount);
             blockInput.Value = false;
         }
     }
